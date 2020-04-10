e009187f838aa8c5b976e86cd01f10c28a340231
Brudihawo <hawo.hoefer98@freenet.de>
2020-04-10 19:21:25 +0200

    Added Grouping of lights

--- hue_controller.py
+++ hue_controller.py
@@ -74,7 +74,8 @@ class HueBridge(NetworkObject):
 
         self.lights = {}
         self.username = None
-        
+        self.groups = {}
+
         try:
             os.mkdir("bridges")
         except FileExistsError:
@@ -86,7 +87,12 @@ class HueBridge(NetworkObject):
                 load = json.load(json_file)
                 self.username = load["username"]
                 self.lights = load["lights"]
+                try:
+                    self.groups = load["groups"]
+                except KeyError:
+                    pass
                 ip = load["ip"]
+                
         # If it does not exist, get information from Hue Bridge
         # Note that the Sync-Button needs to have been pressed for this to work
         except FileNotFoundError:
@@ -146,26 +152,30 @@ class HueBridge(NetworkObject):
         """
         if not self.ip or not self.username or not self.lights or not self.name:
             raise SerializeError("Missing Information", "Cannot serialize uninitialized hue bridge!")
-        out = {"ip": self.ip, "username": self.username, "lights": self.lights}
+        out = {"ip": self.ip, "username": self.username, "lights": self.lights, "groups": self.groups}
         with open(f"bridges/{self.name}.json", "w") as json_file:
             json.dump(out, json_file)
     
-    def set_light_on(self, name):
-        """Turns light on by name
+    def set_light_on(self, names):
+        """Turns lights on by name
+        Args:
+            names: List of light names
         Returns:
             None
         """
         for light_id, light_name in self.lights.items():
-            if light_name == name:
+            if light_name in names:
                 response = self.put(f"api/{self.username}/lights/{light_id}/state", data={"on":True})
 
-    def set_light_off(self, name):
-        """Turns light off by name
+    def set_light_off(self, names):
+        """Turns lights off by name
+        Args:
+            names: List of light names
         Returns:
             None
         """
         for light_id, light_name in self.lights.items():
-            if light_name == name:
+            if light_name in names:
                 response = self.put(f"api/{self.username}/lights/{light_id}/state", data={"on":False})
     
     def get_light_names(self):
@@ -174,6 +184,54 @@ class HueBridge(NetworkObject):
             List of lights (names) connected to hue bridge
         """
         return [self.get_lights()[key] for key in self.get_lights()]
+    
+    def set_light_brightness(self, name):
+        """Sets light brightness by name
+        Returns:
+            None
+        """
+
+    def create_group(self, group_name, light_names):
+        group = []
+        for light_name in light_names:
+            if light_name in self.lights.values():
+                group.append(light_name)
+            else:
+                print(f"Could not find light: {light_name}. Skipped when creating group {group_name}.")
+        self.groups.update({group_name: group})
+        self.serialize()
+
+    def remove_group(self, group_name):
+        try:
+            removed = self.groups.pop(group_name)
+        except KeyError:
+            print(f"Cannot remove non-existant group {group_name}!")
+        else:
+            print(f"Removed group {group_name}({str(removed)})")
+
+        self.serialize()
+    
+    def get_groups(self):
+        return self.groups
+    
+
+    # TODO program group behaviour
+    def turn_group_on(self, group_name):
+        pass
+
+    def turn_group_off(self, group_name):
+        pass
+
+
+def get_next(arr, elem):
+    try:
+        return arr[arr.index(elem) + 1]
+    except IndexError:
+        return None
+
+
+
+
 
 def get_input_params():
     """Gets input parameters
@@ -188,42 +246,70 @@ def get_input_params():
     turn_on = None
     turn_off = None
     showlights = False
+    remove_group = None
+    group = None
+    group_name = None
+    show_groups = False
 
     for index, element in enumerate(sys.argv):
         if element.startswith("-"):
             if element == "--show-bridges":
                 for bridge_file in os.scandir("bridges"):
                     print(bridge_file.name.strip(".json"))
-
+                
             else:
                 if element == "--show-lights":
                     showlights = True
                 if element == "-s":
                     save = True
                 if element == "-i":
-                    ip = sys.argv[sys.argv.index(element) + 1]
-                if element == "-n":
-                    name = sys.argv[sys.argv.index(element) + 1]
+                    ip = get_next(sys.argv, element)
+                if element == "-b":
+                    name = get_next(sys.argv, element)
                 if element == "--on":
-                    turn_on = sys.argv[sys.argv.index(element) + 1]
+                    turn_on = get_next(sys.argv, element).split(";")
                 if element == "--off":
-                    turn_off = sys.argv[sys.argv.index(element) + 1]
-    return save, ip, name, turn_on, turn_off, showlights
+                    turn_off = get_next(sys.argv, element).split(";")
+                if element == "--create-group":
+                    group = get_next(sys.argv, element).split(";")
+                if element == "--group-name":
+                    group_name = get_next(sys.argv, element)
+                if element == "--remove-group":
+                    remove_group = get_next(sys.argv, element)
+                if element == "--show-groups":
+                    show_groups = True
+    return save, ip, name, turn_on, turn_off, showlights, group, group_name, remove_group, show_groups
 
-save, ip, name, turn_on, turn_off, showlights = get_input_params()
+def main():
+    save, ip, name, turn_on, turn_off, showlights, group, group_name, remove_group, show_groups = get_input_params()
+    
+    if name:
+        if ip:
+            bridge = HueBridge(name, ip)
+        else:
+            bridge = HueBridge(name)
+        if save:
+            bridge.serialize()
+    
+        if turn_on:
+            bridge.set_light_on(turn_on)
+        if turn_off:
+            bridge.set_light_off(turn_off)
+        if showlights:
+            for light in bridge.get_light_names():
+                print(light)
+        if group and group_name:
+            try:
+                bridge.create_group(group_name, group)
+            except KeyError:
+                print("fThe group {group_name} already exsists!")
+        if (group and not group_name) or (group_name and not group):
+            print("Use --group-name 'name' and --group 'light_name1;light_name2...' to create a group. Both parameters need to be present.")
+        if remove_group:
+            bridge.remove_group(remove_group)
+        if show_groups:
+            for group_name, lights in bridge.get_groups().items():
+                print(f"{group_name:>20}: {str(lights)}")
 
-if name:
-    if ip:
-        bridge = HueBridge(name, ip)
-    else:
-        bridge = HueBridge(name)
-    if save:
-        bridge.serialize()
-
-    if turn_on:
-        bridge.set_light_on(turn_on)
-    if turn_off:
-        bridge.set_light_off(turn_off)
-    if showlights:
-        for light in bridge.get_light_names():
-            print(light)
+if __name__ == "__main__":
+    main()
